cac9d21ca1896b240ab0d59ad47c0ec17597e935
Zigante <ziganteros@gmail.com>
2025-11-25 16:27:11 +0100

    aggiunta commento

--- main.js
+++ main.js
@@ -36,7 +36,8 @@ const closet=document.getElementById('closet');
     });
   }
 
-  
+
+// Lightbox
   const lightbox=document.getElementById('lightbox');
   const lightboxImg=document.getElementById('lightbox-img');
   const lightboxCaption=document.getElementById('lightbox-caption');
