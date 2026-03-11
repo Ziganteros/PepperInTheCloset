4fa21523dc06ee05e898aaf12b6d062867d55723
Zigante <ziganteros@gmail.com>
2026-03-11 15:42:16 +0100

    aggiunta pagina con app e modifiche ai testi

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded',()=>{
 
   const gallery=document.getElementById('gallery');
   if(gallery){
-    const artworks=[
-      {title:'Posa 1',img:'img/gallery/20241210_235003.jpg',desc:'profilo'},
-      {title:'Posa 2',img:'img/gallery/20250324_093016.jpg',desc:'ciao'},
-      {title:'Posa 3',img:'img/gallery/20250914_165748.jpg',desc:'lo so dovrebbero essere foto di oggetti ma sono esibizionista'}
+    const items=[
+      {title:'Posa 1',img:'img/gallery/20241210_235003.jpg',desc:'Grande pensatore'},
+      {title:'Posa 2',img:'img/gallery/20250324_093016.jpg',desc:'Grandissimo pensatore'},
+      {title:'Posa 3',img:'img/gallery/20250914_165748.jpg',desc:'Sognatore'}
     ];
 
-    artworks.forEach(a=>{
+    items.forEach(a=>{
       const card=document.createElement('div');
       card.className='card';
       card.innerHTML=`<img src="${a.img}" alt="${a.title}"><h3>${a.title}</h3><p>${a.desc}</p>`;
@@ -19,15 +19,15 @@ document.addEventListener('DOMContentLoaded',()=>{
     });
   }
 
-const closet=document.getElementById('closet');
+  const closet=document.getElementById('closet');
   if(closet){
-    const artworks=[
-      {title:'Oggetto 1',img:'img/closet/IMG_0377.jpeg',desc:'la cuiunata'},
-      {title:'Oggetto 2',img:'img/closet/IMG_0378.jpeg',desc:'scarpe'},
-      {title:'Oggetto 3',img:'img/closet/IMG_0379.jpeg',desc:'ancora scarpe'}
+    const items=[
+      {title:'Oggetto 1',img:'img/closet/IMG_0377.jpeg',desc:'scarpe'},
+      {title:'Oggetto 2',img:'img/closet/IMG_0378.jpeg',desc:'ancora scarpe'},
+      {title:'Oggetto 3',img:'img/closet/IMG_0379.jpeg',desc:'e ancora scarpe'}
     ];
 
-    artworks.forEach(a=>{
+    items.forEach(a=>{
       const card=document.createElement('div');
       card.className='card';
       card.innerHTML=`<img src="${a.img}" alt="${a.title}"><h3>${a.title}</h3><p>${a.desc}</p>`;
@@ -36,8 +36,7 @@ const closet=document.getElementById('closet');
     });
   }
 
-
-// Lightbox
+  
   const lightbox=document.getElementById('lightbox');
   const lightboxImg=document.getElementById('lightbox-img');
   const lightboxCaption=document.getElementById('lightbox-caption');
